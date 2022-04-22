0b91c01a22c669f47536d0898937a5b1581e59bc
Aleksander Franczak <aleksanderfranczak@wp.pl>
2022-04-22 13:53:48 +0200

    Add petla FOR

--- lista_zak.py
+++ lista_zak.py
@@ -2,3 +2,5 @@ groc_dict = {
     'Piekarnia' : ['Chleb', 'Pączek', 'Bułki'],
     'Warzywniak' : ['Marchew', 'Seler', 'Rukola']
 }
+for key, value in groc_dict.items():
+    print('Idę do %s, kupuję tu następujące rzeczy: %s' % (key, value))
