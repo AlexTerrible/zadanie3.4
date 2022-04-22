92526fa2bc187b96ecc4652a1426f3c5539f8063
Aleksander Franczak <aleksanderfranczak@wp.pl>
2022-04-22 14:24:41 +0200

    Add Uppercase

--- lista_zak.py
+++ lista_zak.py
@@ -2,5 +2,10 @@ groc_dict = {
     'Piekarnia' : ['Chleb', 'Pączek', 'Bułki'],
     'Warzywniak' : ['Marchew', 'Seler', 'Rukola']
 }
+print(groc_dict['Piekarnia'])
+for i in range (len(groc_dict['Piekarnia'])):
+    groc_dict['Piekarnia'][i] = groc_dict['Piekarnia'][i].upper()
+for i in range (len(groc_dict['Warzywniak'])):
+    groc_dict['Warzywniak'][i] = groc_dict['Warzywniak'][i].upper()
 for key, value in groc_dict.items():
-    print('Idę do %s, kupuję tu następujące rzeczy: %s' % (key, value))
+    print('Idę do %s, kupuję tu następujące rzeczy: %s' % (key.upper(), value))
