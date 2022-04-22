da3adc43bc6a5afb58361fba713367a541f49139
Aleksander Franczak <aleksanderfranczak@wp.pl>
2022-04-22 14:41:46 +0200

    Add Quantity

--- lista_zak.py
+++ lista_zak.py
@@ -2,10 +2,14 @@ groc_dict = {
     'Piekarnia' : ['Chleb', 'Pączek', 'Bułki'],
     'Warzywniak' : ['Marchew', 'Seler', 'Rukola']
 }
+suma = 0
 print(groc_dict['Piekarnia'])
 for i in range (len(groc_dict['Piekarnia'])):
     groc_dict['Piekarnia'][i] = groc_dict['Piekarnia'][i].upper()
 for i in range (len(groc_dict['Warzywniak'])):
     groc_dict['Warzywniak'][i] = groc_dict['Warzywniak'][i].upper()
 for key, value in groc_dict.items():
+    for j in value:
+        suma = suma + 1
     print('Idę do %s, kupuję tu następujące rzeczy: %s' % (key.upper(), value))
+print ('W sumie kupuje %s produktów.' % (suma))
